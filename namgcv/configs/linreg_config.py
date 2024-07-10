c21205a72af09adaa48d7f9039d43417f8a7d999
AFThielmann <antonthielmann@t-online.de>
2024-07-10 22:46:15 +0200

    config: add defaults for base neural models

---
+++ namgcv/configs/linreg_config.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+import torch.nn as nn
+
+
+@dataclass
+class DefaultLinRegConfig:
+    """
+    Configuration class for the default LinReg with predefined hyperparameters.
+
+    Parameters
+    ----------
+    lr : float, default=1e-04
+        Learning rate for the optimizer.
+    lr_patience : int, default=10
+        Number of epochs with no improvement after which learning rate will be reduced.
+    weight_decay : float, default=1e-06
+        Weight decay (L2 penalty) for the optimizer.
+    lr_factor : float, default=0.1
+        Factor by which the learning rate will be reduced.
+    """
+
+    lr: float = 1e-04
+    lr_patience: int = 10
+    weight_decay: float = 1e-06
+    lr_factor: float = 0.1
