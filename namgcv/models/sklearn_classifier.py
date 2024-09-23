f32956d6b2e577e42ea9119305d1fb3964992682
AFThielmann <antonthielmann@t-online.de>
2024-09-23 12:59:00 +0200

    cat prepro in model args

--- namgcv/models/sklearn_classifier.py
+++ namgcv/models/sklearn_classifier.py
@@ -16,6 +16,7 @@ class SklearnBaseClassifier(BaseEstimator):
         preprocessor_arg_names = [
             "n_bins",
             "numerical_preprocessing",
+            "categorical_preprocessing",
             "use_decision_tree_bins",
             "binning_strategy",
             "task",
